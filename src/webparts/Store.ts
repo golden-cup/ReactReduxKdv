69050d89cc2da5c94044991fc5ad0f8e6402c83f
unknown <meas400@MEDIEVAL.ML>
2019-02-17 18:49:32 +0300

    redux debugging added

--- src/webparts/Store.ts
+++ src/webparts/Store.ts
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware, AnyAction, Store} from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducer from './reactReduxSpfx/reducers/Root';
 
 const middleware = applyMiddleware(thunk);
-const store = createStore(reducer, middleware);
+const store = createStore(reducer, composeWithDevTools(
+    applyMiddleware(thunk)));
 
 store.subscribe(() => {
     console.log('subscribe', store.getState());
